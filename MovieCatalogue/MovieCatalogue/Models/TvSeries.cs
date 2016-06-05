a6df01cb51e49e723cd34a75942e7deea53c84ee
dimitar-donov075 <dimitar.donov96@gmail.com>
2016-06-05 16:15:18 +0300

    Created Movies/TvSeries/Animes

---
+++ MovieCatalogue/MovieCatalogue/Models/TvSeries.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace MovieCatalogue.Models
+{
+    public class TvSeries
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public int Season { get; set; }
+        public int Episode { get; set; }
+    }
+    public class TvSeriesDBContext : DbContext
+    {
+        public DbSet<TvSeries> Series { get; set; }
+    }
+}
